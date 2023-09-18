c9e19e676977383f97279dd9c6bd9c84d57fcf9c
pingthSK <141994287+pingthSK@users.noreply.github.com>
2023-09-18 14:42:47 +0700

    Update ssss.sql

--- ssss.sql
+++ ssss.sql
@@ -1,2 +1,2 @@
-INSERT INTO Data (outsidetitle, insidetitle, describtion, author)
-VALUES ('DIY picture','Order now!','beauitiful decorated picture order: https://shopee.co.th/product/1075439252/19884204582/', 'ping');
+INSERT INTO post (name, room, title, description, price, productname)
+VALUES ('DIY picture','202', 'Order now!','beauitiful decorated picture order: https://shopee.co.th/product/1075439252/19884204582/', 10, 'DIY picture');
